a9764c3100ec4b67a154ee8530d9a6fc58effd3e
Stéphan Zych <stephan@zych.be>
2026-02-21 22:50:14 +0100

    docs: note completion setup

--- completions/cdx.zsh
+++ completions/cdx.zsh
@@ -6,10 +6,49 @@ _cdx() {
     '1:directory:_directories'
 }
 
+_up_levels() {
+  compadd -Q -- {1..9}
+}
+
+_up_level_or_dir() {
+  _alternative \
+    'levels:level:_up_levels' \
+    'dirs:directory:_directories'
+}
+
+_up_subpath() {
+  local cur="${words[$CURRENT]}"
+  local num="${cur%%/*}"
+  local rest="${cur#*/}"
+  local base=""
+  local i
+  local -a dirs
+
+  for (( i = 1; i <= num; i++ )); do
+    base+="../"
+  done
+  base+="$rest"
+
+  dirs=(${base}*(/N))
+  dirs=(${dirs#$base})
+  dirs=(${dirs/#/${num}/})
+  compadd -Q -- $dirs
+}
+
 _up() {
   _arguments \
     '-i[inspect mode]' \
-    '1:N[/subpath]:_directories'
+    '1:level[/subpath]:->target'
+
+  case "$state" in
+    target)
+      if [[ "${words[$CURRENT]}" == <->/* ]]; then
+        _up_subpath
+      else
+        _up_level_or_dir
+      fi
+      ;;
+  esac
 }
 
 case "$service" in
